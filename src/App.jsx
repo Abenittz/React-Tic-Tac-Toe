7eb1589448055efa258c8da8075c41505d612e90
Abenittz <136760687+Abenittz@users.noreply.github.com>
2024-03-02 10:11:07 +0300

    creating the board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function Square() {
+function Square({ value }) {
+  return (
+    <button className="square border-2 border-black bg-transparent px-4 py-2">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
-      <button className="square">X</button>
+      <div className="flex justify-center items-center flex-col h-screen">
+        <div className="">
+          <Square value="1" />
+          <Square value="2" />
+          <Square value="3" />
+        </div>
+        <div className="">
+          <Square value="4" />
+          <Square value="5" />
+          <Square value="6" />
+        </div>
+        <div className="">
+          <Square value="7" />
+          <Square value="8" />
+          <Square value="9" />
+        </div>
+      </div>
     </>
   );
 }
